c3274e0781eb80901ced5f3c75894b1b8df1333a
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2023-04-04 14:00:37 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='croto',
+    version='0.6',
+    license='MIT',
+    author="Defee",
+    author_email='fdiaznemeth@example.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Defeeeee/croto',
+    keywords='compiled language',
+    install_requires=[
+          'scikit-learn',
+      ],
+
+)
